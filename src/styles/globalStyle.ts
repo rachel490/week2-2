3b02e3a7572f977b615b3207b4a90ce6481f3b9b
rachel490 <ccori12@naver.com>
2022-02-10 22:34:40 +0900

    Style: Theme 및 Global Style 설정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,10 +2,28 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap");
+
   ${reset}
 
   * {
     box-sizing: border-box;
+    font-size: 14px;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    color: ${({ theme }) => theme.color.black};
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
   }
 
 `;
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,6 +2,29 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    color: string;
+    color: {
+      primaryPurple: string;
+      lightPurple: string;
+      whiteGrey: string;
+      white: string;
+      darkGrey: string;
+      mediumGrey: string;
+			black: string;
+    };
+    boxShadow: string;
+    border: {
+      radius: string;
+    };
+    fontSize: {
+      xsmall: string;
+      small: string;
+      original: string;
+      medium: string;
+    };
+    fontWeight: {
+      normal: number;
+      bolder: number;
+      bold: number;
+    };
   }
 }
