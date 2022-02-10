977436c59983e0e7ce2bddf45cc13cd5342e41ed
rachel490 <ccori12@naver.com>
2022-02-10 19:14:56 +0900

    Style: global style 및 theme 초기세팅

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store/store';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/defaultTheme';
+import GlobalStyle from 'styles/globalStyle';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/styles/defaultTheme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  color: 'black',
+};
+
+export default theme;
