40b29831b2059a434e8c18f736b2309fe4ad83bc
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-02-12 00:07:01 +0900

    Feat: 채팅창 상시 하단 위치 기능 구현

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import ChatBubble from './ChatBubble';
+import { ChatListView } from './styled';
 
 function ChatList() {
   const messages = useSelector((state: RootState) => state.messenger.messages);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = useCallback(() => {
+    if (messages) {
+      scrollRef.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest',
+      });
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, scrollToBottom]);
 
   return (
-    <div>
+    <ChatListView ref={scrollRef}>
       {messages.map((message) => (
         <ChatBubble key={message.id} message={message} mine />
       ))}
-    </div>
+    </ChatListView>
   );
 }
 
